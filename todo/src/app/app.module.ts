5168661b215c345ac191f8f053720a360d31fcb3
sarlury <jemy.sarlury@gmail.com>
2023-09-18 15:23:58 +0700

    add pages

--- todo/src/app/app.module.ts
+++ todo/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoDashboardComponent } from './pages/todo-dashboard/todo-dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoDashboardComponent
   ],
   imports: [
     BrowserModule,
---
+++ todo/src/app/pages/todo-dashboard/todo-dashboard.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-dashboard',
+  templateUrl: './todo-dashboard.component.html',
+  styleUrls: ['./todo-dashboard.component.scss']
+})
+export class TodoDashboardComponent {
+
+}
